04c90c632a74d6425ee23e45725f2bd105ed8c90
Daniel <danik5311@gmail.com>
2021-01-29 18:43:50 +0300

    update payment model to user reference

--- src/Admin/components/Payment.js
+++ src/Admin/components/Payment.js
@@ -13,27 +13,30 @@ import {
     TextInput,
     SelectInput,
     ReferenceField,
-    ReferenceInput
+    ReferenceInput,
+    FunctionField
 } from 'react-admin';
 
 export const PaymentList = (props) => (
     <List {...props}>
         <Datagrid rowClick="show">
             <TextField source="id" />
-            <TextField source="user.username" />
-            <TextField source="September" />
-            <TextField source="October" />
-            <TextField source="November" />
-            <TextField source="December" />
-            <TextField source="January" />
-            <TextField source="February" />
-            <TextField source="March" />
-            <TextField source="March" />
-            <TextField source="April" />
-            <TextField source="May" />
-            <TextField source="June" />
-            <TextField source="July" />
-            <TextField source="August" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <FunctionField render={record => `${record.name} ${record.middleName} ${record.surname}`} />
+            </ReferenceField>
+            <TextField source="september" />
+            <TextField source="october" />
+            <TextField source="november" />
+            <TextField source="december" />
+            <TextField source="january" />
+            <TextField source="february" />
+            <TextField source="march" />
+            <TextField source="march" />
+            <TextField source="april" />
+            <TextField source="may" />
+            <TextField source="june" />
+            <TextField source="july" />
+            <TextField source="august" />
 
             <EditButton />
             <DeleteButton />
@@ -46,23 +49,24 @@ export const PaymentShow = (props) => (
         <SimpleShowLayout>
             <TextField source="id" />
 
-            <ReferenceField label="User" reference="users" source="userId">
-                <TextField source="name" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <FunctionField render={record => `${record.name} ${record.middleName} ${record.surname}`} />
             </ReferenceField>
 
-            <TextField source="September" />
-            <TextField source="October" />
-            <TextField source="November" />
-            <TextField source="December" />
-            <TextField source="January" />
-            <TextField source="February" />
-            <TextField source="March" />
-            <TextField source="March" />
-            <TextField source="April" />
-            <TextField source="May" />
-            <TextField source="June" />
-            <TextField source="July" />
-            <TextField source="August" />
+            <TextField source="september" />
+            <TextField source="october" />
+            <TextField source="november" />
+            <TextField source="december" />
+            <TextField source="january" />
+            <TextField source="february" />
+            <TextField source="march" />
+            <TextField source="march" />
+            <TextField source="april" />
+            <TextField source="may" />
+            <TextField source="june" />
+            <TextField source="july" />
+            <TextField source="august" />
+            <EditButton/>
         </SimpleShowLayout>
     </Show>
 );
@@ -71,22 +75,22 @@ export const PaymentEdit = (props) => (
     <Edit {...props}>
         <SimpleForm>
             <TextInput disabled source="id" />
-            <ReferenceField label="User" reference="users" source="userId">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextInput source="September" />
-            <TextInput source="October" />
-            <TextInput source="November" />
-            <TextInput source="December" />
-            <TextInput source="January" />
-            <TextInput source="February" />
-            <TextInput source="March" />
-            <TextInput source="March" />
-            <TextInput source="April" />
-            <TextInput source="May" />
-            <TextInput source="June" />
-            <TextInput source="July" />
-            <TextInput source="August" />
+            <ReferenceInput label="user" source="userId" reference="users">
+                <SelectInput optionText={record => `${record.name} ${record.surname}`} />
+            </ReferenceInput>
+            <TextInput source="september" />
+            <TextInput source="october" />
+            <TextInput source="november" />
+            <TextInput source="december" />
+            <TextInput source="january" />
+            <TextInput source="february" />
+            <TextInput source="march" />
+            <TextInput source="march" />
+            <TextInput source="april" />
+            <TextInput source="may" />
+            <TextInput source="june" />
+            <TextInput source="july" />
+            <TextInput source="august" />
         </SimpleForm>
     </Edit>
 );
